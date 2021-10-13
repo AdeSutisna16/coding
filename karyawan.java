ddb5e1e0a7dc0751940a1538f54794fccba189c4
AdeSutisna16 <92408599+AdeSutisna16@users.noreply.github.com>
2021-10-13 08:48:54 +0700

    Add files via upload

---
+++ karyawan.java
@@ -0,0 +1,26 @@
+public class karyawan {
+int noid;
+String nama;
+int usia;
+
+public void setNoid(int i) {
+	noinduk=i;
+}
+public void setNama(String i) {
+	nama=i;
+}
+public void setUsia(int i) {
+	usia=i;
+}
+public class Test {
+	public static void main (String args [] ) {
+		karyawan=new karyawan();
+		siswa.setNoid(1451);
+		siswa.setNama("jamal");
+		siswa.setUsia(28);
+	System.out.println (karyawan.noid);
+	System.out.println (karyawan.nama);
+	System.out.println (karyawan.usia);
+}
+}
+}
